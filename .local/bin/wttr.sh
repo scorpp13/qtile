3d0294d286b90bba1ff5ad951a7f4210f2b4d924
Alexey Scorpionov <scorpp13@gmail.com>
2025-05-08 10:01:36 +0200

    update

--- .local/bin/wttr.sh
+++ .local/bin/wttr.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-MESSAGE=$(gum style --foreground="10" "Press Enter to continue")
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NONE='\033[0m'
+
 kitty "$SHELL" -c "curl 'ru.wttr.in/Kaliningrad?format=v2d'; \
-echo ''
-read -rp '$MESSAGE' </dev/tty"
+echo -e '$GREEN'
+read -rp 'Press Enter to continue' </dev/tty"
